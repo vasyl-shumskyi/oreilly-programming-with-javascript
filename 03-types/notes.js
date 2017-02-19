1ad48a9774ff9ada575db798e01d69749a60f16b
Vasyl <vasyl.shumskyi@gmail.com>
2017-02-19 16:37:29 +0200

    Types - Theory

--- 03-types/notes.js
+++ 03-types/notes.js
@@ -1,2 +1,23 @@
 // your notes here!
 console.log("hello world!");
+console.log("this is the types section!")
+
+var isNumber = function (potentialNumber) {
+  return typeof potentialNumber === "number";
+};
+
+var isPositiveNumber = function (number) {
+  return isNumber(number) && number > 0;
+};
+
+var isYear = function(year) {
+  return isPositiveNumber(year) && year <= 9999;
+}
+
+var isTweet = function (potentialTweet) {
+  return potentialTweet.length <= 140;
+}
+
+var isTweetWithWord = function (tweet, word) {
+  return isTweet(tweet) && tweet.indexOf(word) > -1;
+}
