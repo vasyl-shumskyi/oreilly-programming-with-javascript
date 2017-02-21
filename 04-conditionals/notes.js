d5effc663a06d24ad09c367c5d241f9dff133572
Vasyl <vasyl.shumskyi@gmail.com>
2017-02-21 17:55:51 +0200

    Conditionals - Theory - Cascaded & Nested if / else

--- 04-conditionals/notes.js
+++ 04-conditionals/notes.js
@@ -55,9 +55,119 @@ var improveTweet2 = function  (tweet) {
 
   var result = tweet;
 
-  if ( tweet.toLowerCase().indexOf("lol") === -1 ) { 
+  if ( tweet.toLowerCase().indexOf("lol") === -1 ) {
     result = tweet + ' lol';
   }
 
   return result;
+}
+
+
+// cascaded if / else
+
+var greetingByHour = function (hour) {
+    var result;
+
+    if (0 <= hour && hour <= 5) {
+        result = "Wow, it's early!";
+    } else if (5 < hour && hour <= 12) {
+        result = "Good morning!";
+    } else if (12 < hour && hour <= 17) {
+        result = "Good afternoon!";
+    } else if (17 < hour && hour <= 20) {
+        result = "Good evenening!";
+    } else if (20 < hour && hour <= 24) {
+        result = "Shouldn't you be in bed?";
+    } else {
+          result = "Oh gosh, this is awkward - that's not a time.";
+    }
+
+    return result;
+};
+
+// Try out cascaded if / else
+
+var awesomeTweet = function (tweet) {
+
+    var result;
+    length = tweet.length;
+
+
+    if (0 < length && length < 50) {
+        result = "Not great";
+    } else if (50 <= length && length < 100) {
+        result = "sorta-good";
+    } else if (100 <= length && length <= 150) {
+        result = "awesome";
+    } else {
+          result = "invalid tweet length";
+    }
+
+    return result + '|' + length;
+};
+
+// Nested if / else
+
+var maxOfThree = function (a,b,c) {
+  var result;
+
+  if (a > b) {
+    if (a > c) {
+        result = a;
+    } else {
+        result = c;
+    }
+  } else {
+      if (b > c) {
+        result = b;
+      } else {
+        result = c;
+      }
+  }
+
+  return result;
+}
+
+// nested if/else formated
+
+var maxOfThree2 = function (a,b,c) {
+  var result;
+
+  if (a > b) {
+      if  (a > c)     { result = a; }
+      else            { result = c; }
+  } else {
+      if  (b > c)     { result = b; }
+      else            { result = c; }
+  }
+
+  return result;
+}
+
+// nested to cascaded if / else
+
+var maxOfThree3 = function (a,b,c) {
+
+  var result;
+
+  if      (a >= b && a >= c)  { result = a };
+  else if (b >= a && b >= c)  { result = b };
+  else                        { result = c };
+
+  return result;
+
+}
+
+// nested if/else BAD alternative
+
+var maxOfThree4 = function (a,b,c) {
+
+  var result;
+
+  if (a >= b && a >= c) { result = a };
+  if (b >= a && b >= c) { result = b };
+  if (c >= a && c >= b) { result = c };
+
+  return result;
+
 }
