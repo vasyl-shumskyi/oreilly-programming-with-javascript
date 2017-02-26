cf9432c9d2ee247eff3754256a1252547f052600
Vasyl <vasyl.shumskyi@gmail.com>
2017-02-26 10:34:13 +0200

    Conditionals - Practice 3

--- 04-conditionals/practice.js
+++ 04-conditionals/practice.js
@@ -1,3 +1,21 @@
+///////////////
+// helper functions that are used throughout
+var isString = function (val) {
+    return typeof val === "string";
+};
+
+var isHTMLElement = function (str) {
+    var openTag = str.substring(str.indexOf("<") + 1, str.indexOf(">"));
+    var closeTag = str.substring(str.lastIndexOf("</") + 2, str.lastIndexOf(">"));
+    return str.charAt(0) === "<" && str.charAt(str.length - 1) === ">" && openTag === closeTag;
+};
+
+var randUpTo = function (n) {
+    return Math.floor(Math.random() * n);
+};
+///////////////
+
+
 // We'll consider a password's strength to be "strong" if it is at least 10
 // characters long. If it's between 7 and 10 characters long, we'll consider it to
 // have "medium" strength, and if it's less than 7 characters, we'll say it is a
@@ -24,6 +42,21 @@ var passwordStrength = function (password) {
   return result;
 };
 
+////////////////////
+var passwordStrength = function (password) {
+    var result;
+
+    if (password.length < 7) {
+        result = "weak";
+    } else if (password.length >= 7 && password.length < 10) {
+        result = "medium";
+    } else {
+        result = "strong";
+    }
+
+    return result;
+};
+////////////////////
 
 // A year is a leap year if it is divisible by 4, unless it is also divisible by
 // 100 in which case it is not a leap year unless it is also divisible by
@@ -64,6 +97,57 @@ var isLeapYear = function (year) {
 
 };
 
+///////////////////////////
+// with nested if statements
+var isLeapYear2 = function (year) {
+    var result = false;
+
+    if (typeof year !== "number") {
+        throw "argument to isLeapYear must be a number";
+    }
+
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 !== 0) {
+            result = false;
+        } else {
+            result = true;
+        }
+    } else {
+        result = false;
+    }
+
+    return result;
+};
+
+// a flatter version
+isLeapYear3 = function (year) {
+    var result = false;
+
+    if (typeof year !== "number") {
+        throw "argument to isLeapYear must be a number";
+    }
+
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
+        result = false;
+    } else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+        result = true;
+    } else if (year % 4 === 0) {
+        result = true;
+    }
+
+    return result;
+};
+
+// !!! the simplest version
+isLeapYear4 = function (year) {
+    if (typeof year !== "number") {
+        throw "argument to isLeapYear must be a number";
+    }
+
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+///////////////////////////
+
 
 // Write a function that accepts three strings and input, and returns the one
 // that would come earliest in the dictionary.
@@ -99,6 +183,21 @@ var firstInDictionary = function (string1, string2, string3) {
 
 };
 
+//////////////////////////////////////
+var firstInDictionary2 = function (a, b, c) {
+    if (!isString(a) || !isString(b) || !isString(c)) {
+        throw "ALL THREE ARGS MUST BE STRINGS!";
+    }
+
+    if (a <= b && a <= c) {
+        return a;
+    } else if (b <= a && b <= c) {
+        return b;
+    } else {
+        return c;
+    }
+};
+//////////////////////////////////////
 
 // Write a function that extracts a tag from a string representing an HTML
 // element, but throws an error if the string is not an HTML element. You may reuse
@@ -123,6 +222,16 @@ var getTagName = function (html) {
 
 };
 
+///////////////////////////
+var getTagName = function (elt) {
+    if (!isHTMLElement(elt)) {
+        throw "Error: Not an HTML Element!";
+    }
+
+    return elt.slice(1, elt.indexOf(">"));
+};
+///////////////////////////
+
 
 // Re-implement our improveTweet function so it can generate any of lol, omg,
 // lmao, and rofl.
@@ -136,9 +245,9 @@ var improveTweet = function (tweet) {
   if      (n === 1)   { word = " lol";  }
   else if (n === 2)   { word = " omg";  }
   else if (n === 3)   { word = " lmao"; }
-  else if (n === 4)   { word = " rofl"; }
+  else                { word = " rofl"; }   // no need to be specific about (n === 4)
 
- if (tweet.indexOf(word) === -1) {
+ if (tweet.toLowerCase().indexOf(word) === -1) {
    result = tweet + word;
  } else {
    result = tweet;
@@ -148,6 +257,31 @@ var improveTweet = function (tweet) {
 
 };
 
+
+////////////////////////////////
+var improveTweet = function (tweet) {
+    // generate either 0, 1, 2, or 3 using the randUpTo function defined above
+    var random = randUpTo(4);
+    var result = tweet;
+    var expression;
+
+    if (random === 0) {
+        expression = "lol";
+    } else if (random === 1) {
+        expression = "omg";
+    } else if (random === 2) {
+        expression = "lmao";
+    } else {
+        expression = "rofl";
+    }
+
+    if (result.indexOf(expression) === -1 && result.indexOf(expression.toUpperCase()) === -1) {
+        result = result + " " + expression;
+    }
+
+    return result;
+};
+////////////////////////////////
 
 
 // Write a function called `isQuestion` that returns true if the input is a
@@ -162,6 +296,13 @@ var isQuestion = function (sentence) {
   }
 
 };
+
+/////////////////////
+var isQuestion2 = function (sentence) {
+    // this uses the helper isString function which you can find above
+    return isString(sentence) && sentence.charAt(sentence.length - 1) === "?";
+};
+/////////////////////
 
 
 // The Magic 8 Ball is a classic toy that allows you to ask a yes/no
@@ -202,6 +343,34 @@ var magic8Ball = function (question) {
   return answer;
 
 };
+
+///////////////////
+var magic8Ball2 = function (question) {
+    if (!isQuestion(question)) {
+        throw "THAT DOESN'T SOUND LIKE A QUESTION!";
+    }
+
+    // reuse the randUpTo function defined above
+    var number = randUpTo(4);
+    var result;
+
+    if (number === 0) {
+        result = "Yes!";
+    } else if (number === 1) {
+        result = "Definitely!";
+    } else if (number === 2) {
+        result = "Maybe...";
+    } else if (number === 3) {
+        result = "No :(";
+    } else {
+        // if our rand num generator is right, this
+        // should never happen
+        throw "Something went horribly wrong!";
+    }
+
+    return result;
+};
+///////////////////
 
 
 // Suppose we wanted to randomly interject "-lol-" or "-omg-" into a random
@@ -268,6 +437,20 @@ var interjectAt = function (intergaction, index, string) {
 
 }
 
+///////////////////
+var interjectAt3 = function (interjection, index, tweet) {
+    if (!isString(interjection) || typeof index !== "number" || !isString(tweet)) {
+        throw "expected first arg to be a string, second arg to be a number and third arg to be a string";
+    }
+
+    if (index > tweet.length - 1) {
+        throw "the string doesn't have that many letters!";
+    }
+
+    return tweet.slice(0,index) + "-" + interjection + "-" + tweet.slice(index);
+};
+///////////////////
+
 // Now that you have a robust function to do your interjection, your actual
 // `randomInterjection` function consists of generating a random message and a
 // random location within the string, and then calling into the `interjectAt`
@@ -288,3 +471,25 @@ var randomInterject = function (inputStr) {
   return interjectAt(word, randomIndex, inputStr)
 
 };
+
+
+//////////////////////////
+var randomInterject2 = function (tweet) {
+    if (!isString(tweet)) {
+        throw "the input should be a string!";
+    }
+
+    // reuse the randUpTo function defined above
+    var randomMsgNum = randUpTo(2);
+    var randomIndex = randUpTo(tweet.length);
+    var result;
+
+    if (randomMsgNum === 0) {
+        result = interjectAt("-lol-", randomIndex, tweet);
+    } else {
+        result = interjectAt("-omg-", randomIndex, tweet);
+    }
+
+    return result;
+};
+//////////////////////////
