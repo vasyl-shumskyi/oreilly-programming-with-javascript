21ccc61c442e7bc1551a96847c4a66d037c10a98
Vasyl <vasyl.shumskyi@gmail.com>
2017-03-01 17:35:44 +0200

    Loops - Theory 2

--- 05-loops/notes.js
+++ 05-loops/notes.js
@@ -67,3 +67,139 @@ for (number = 0; number <= 100; number = number + 1) {
     console.log("number is " + number);
   }
 }
+
+////////////////////////////////////////////////
+
+var SumFirstNumbers = function(number) {
+
+    var n;
+    var sum = 0;
+
+    for (n = 1; n <= number; n = n + 1) {
+      sum = sum + n;
+    }
+
+    return sum;
+}
+
+
+var LargestDivisor = function(number) {
+  var divisor;
+  var n;
+// n < number should be strictly less, because n <= number will give us the number itself as divisor
+  for (n = 1; n < number; n = n + 1) {
+    if (number % n === 0) {
+      divisor = n;
+    }
+  }
+
+  return divisor;
+}
+
+////////////////////////////////////////////////
+
+var greeting = "hello world!";
+console.log(greeting);
+
+var index
+
+for (index = 0; index < greeting.length; index = index + 1) {
+  console.log(greeting.charAt(index));
+}
+// ^ will print out each letter in a raw
+
+var removeVowels = function (string) {
+
+  if (typeof string !== "string") {
+    throw "Only strings allowed!";
+  }
+
+  var result = '';
+  var index;
+
+  for (index = 0; index < string.length; index = index + 1) {
+
+    letter = string.charAt(index);
+    letterLower = letter.toLowerCase();
+
+    if (letterLower !== 'a' &&
+        letterLower !== 'e' &&
+        letterLower !== 'i' &&
+        letterLower !== 'o' &&
+        letterLower !== 'u') {
+
+      result = result + letter;
+    }
+  }
+
+  return result;
+
+}
+
+////////////////////////////////////////////////
+
+var FirstLowerCaseVowel = function (string) {
+
+  if (typeof string !== "string") {
+    throw "Input should be the string!"
+  }
+
+  var result = '';
+  var index;
+
+  for (index = 0; index < string.length && result === ''; index = index + 1) {    // !!! && result === ''
+//  for (index = 0; index < string.length; index = index + 1) {                   // 2nd approach
+
+    var letter = string.charAt(index);
+
+    if (letter === 'a' ||
+        letter === 'e' ||
+        letter === 'i' ||
+        letter === 'o' ||
+        letter === 'u') {
+
+          result = letter;
+//          break;                                                                // 2nd approach
+//          return result;                                                        // 3rd approach
+        };
+  };
+
+  return result;
+}
+
+
+
+var FirstLowerCaseLetter = function(string) {
+
+  if (typeof string !== "string") {
+    throw "Input should be the string";
+  }
+
+  var result = '';
+  var index;
+
+  for (index = 0; index < string.length && result === ''; index = index + 1) {
+
+    letter = string.charAt(index);
+
+    if ("a" <= letter && letter <= "z") {
+      result = letter;
+    }
+  }
+
+  return result;
+}
+
+////////////////////////////////////////////////
+
+var countDown = function(number) {
+
+  if (typeof number !== "number") {
+    throw "Only positive integers allowed!"
+  }
+
+  var n;
+  for (n = number; n >= 0; n = n - 1 ) {
+    console.log(n);
+  }
+}
