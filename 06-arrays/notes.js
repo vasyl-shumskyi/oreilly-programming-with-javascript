4a6a711741597239efd5de3bc75717f037f669b2
Vasyl <vasyl.shumskyi@gmail.com>
2017-03-04 11:47:23 +0200

    Arrays - Theory

--- 06-arrays/notes.js
+++ 06-arrays/notes.js
@@ -1,2 +1,31 @@
 // your notes here!
 console.log("hello world!");
+console.log("arrays");
+
+// START - Arrays - Overview & Basic Operations TRY OUTS :
+// 1
+var positiveEvenNumbers = [];
+var limit = 10;
+var n;
+
+for (n = 0; n < limit; n = n + 1) {
+  if (n % 2 === 0) {
+    positiveEvenNumbers.push(n);
+  };
+};
+
+for (n = 0; n < positiveEvenNumbers.length; n = n + 1) {
+  console.log(positiveEvenNumbers[n]);
+};
+
+// 2
+var firstPositiveIntegers = [];
+var limit = 15;
+var n;
+
+for (n = 1; n <= limit; n = n + 1) {
+  firstPositiveIntegers.push(n);
+}
+
+console.log(firstPositiveIntegers);
+// END - Arrays - Overview & Basic Operations TRY OUTS:
