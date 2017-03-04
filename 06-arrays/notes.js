eced779fb61ec0a7e2f5738435bbfc525eb2d95c
Vasyl <vasyl.shumskyi@gmail.com>
2017-03-04 14:09:52 +0200

    Arrays - Theory 2

--- 06-arrays/notes.js
+++ 06-arrays/notes.js
@@ -29,3 +29,92 @@ for (n = 1; n <= limit; n = n + 1) {
 
 console.log(firstPositiveIntegers);
 // END - Arrays - Overview & Basic Operations TRY OUTS:
+
+
+// START - Arrays - More Examples
+
+var printArrayValues = function(list) {
+  // list is array synonim
+  var index;
+
+  for (index = 0; index < list.length; index = index + 1) {
+    console.log("The element is " + list[index ]);
+  }
+}
+
+////////////////////////////////////////////////
+
+var sumArrayValues = function(array) {
+  var index;
+  var sum = 0;
+
+  for (index = 0; index < array.length; index = index + 1) {
+    sum = sum + array[index];
+  }
+
+  return sum;
+
+};
+
+////////////////////////////////////////////////
+// Remove vowels from all array elements
+
+var isVowel = function(letter) {
+
+  letterL = letter.toLowerCase();
+
+  trueFalse = letterL === "a" || letterL === "e" ||     // !!! we can use return instead of 'trueFalse =''
+              letterL === "i" || letterL === "o" ||
+              letterL === "u";
+
+  return trueFalse;
+}
+
+
+var removeVowelsFromStr = function(string) {
+
+  var result = '';
+  var n;
+
+  for (n = 0; n < string.length; n = n + 1) {
+
+    letter = string.charAt(n);                          // !!! better to use 'letter = string[n];'
+
+    if (!isVowel(letter)) {
+          result = result + letter;
+    };
+  };
+
+  return result;
+}
+
+
+var removeVowelsFromArray = function(array) {
+  var index;
+  var resultArray = [];
+
+  for (index = 0; index < array.length; index = index + 1) {
+
+    newElement = removeVowelsFromStr(array[index]);
+    resultArray.push(newElement);
+
+  }
+  return resultArray;
+}
+
+////////////////////////////////////////////////
+// Find smallest number in array
+
+var smallestArrayNum = function(array) {
+
+  var index;
+  var min = array[0];
+
+  for (index = 1; index < array.length; index = index + 1) {
+    if (array[index] < min) { min = array[index] };               // ! min = Math.min(min, array[index]); will also works
+  };
+
+  return min;
+}
+
+// END - Arrays - More Examples
